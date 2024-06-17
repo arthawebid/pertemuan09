07684a643be36e7142dc2392b7ae992c8b2fcd07
MhSTIKI <arthawebid@gmail.com>
2024-06-17 14:32:52 +0800

    Pertemuan 9

---
+++ src/pertemuan09/Pertemuan09.java
@@ -0,0 +1,25 @@
+package pertemuan09;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author INSTIKI
+ * TGL: 2024-05-16
+ */
+public class Pertemuan09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+//        new jForm1().setVisible(true);
+        
+        jForm1 jf = new jForm1();
+        jf.setResizable(false);
+        jf.setAlwaysOnTop(true);
+        jf.setVisible(true);
+        
+    }
+    
+}
---
+++ src/pertemuan09/koneksi.java
@@ -0,0 +1,23 @@
+package pertemuan09;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author INSTIKI
+ * TGL: 2024-05-30
+ */
+public class koneksi {
+    public static Connection buatkoneks(){
+        Connection cnn = null;
+        try{
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cnn = DriverManager.getConnection("jdbc:mysql://localhost:3306/temanbaik","root","");
+            
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println("Koneksi ke DBMS MySQL gagal");
+        }
+        return cnn;
+    }
+}
